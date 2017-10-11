f8b53a2d3bc67bbc512dfe0aa84f48add01284e9
vadaskovich <nick.shipovalov@gmail.com>
2017-10-11 19:46:05 +0300

    init assertions that throw

--- Sources/Common/throw_assert.cpp
+++ Sources/Common/throw_assert.cpp
@@ -1,9 +1,31 @@
 //
 //  throw_assert.cpp
-//  Graph_Test
+//  Common
 //
 //  Created by Bro on 10/11/17.
 //  Copyright © 2017 vadaskovich. All rights reserved.
 //
 
+#include <stdexcept>
+#include <cstdio>
+#include <cstdarg>
+
 #include "throw_assert.hpp"
+
+// I really can not say whether it is safe or not
+void __throw_assert(bool condition, const char* conditionString, ...) {
+  if (condition == false) {
+    char format_str[256];
+    char message_str[256];
+  
+    va_list args;
+    
+    va_start(args, conditionString);
+    vsnprintf(format_str, 256, "%s", args);
+    vsnprintf(message_str, 256, format_str, args);
+    va_end(args);
+
+    throw std::runtime_error(message_str);
+  }
+}
+
--- Sources/Common/throw_assert.hpp
+++ Sources/Common/throw_assert.hpp
@@ -1,6 +1,6 @@
 //
 //  throw_assert.hpp
-//  Graph_Test
+//  Common
 //
 //  Created by Bro on 10/11/17.
 //  Copyright © 2017 vadaskovich. All rights reserved.
@@ -9,6 +9,13 @@
 #ifndef throw_assert_hpp
 #define throw_assert_hpp
 
-#include <stdio.h>
+void __throw_assert(bool condition, const char* conditionString, ...);
+
+// Asserts that throw an exception if condition is false
+#define throw_assert_explain(__condition__, ...) \
+__throw_assert( (bool)(__condition__), #__condition__, __VA_ARGS__ )
+
+#define throw_assert(__condition__) \
+__throw_assert( (bool)(__condition__), #__condition__ )
 
 #endif /* throw_assert_hpp */
